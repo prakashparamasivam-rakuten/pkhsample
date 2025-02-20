3221154d59903bfa215b70e5035891d8bdf1f606
prakash.paramasivam <ts-praka.paramasivam@rakuten.com>
2025-02-20 21:11:14 +0900

    adding redux and slice sample with async increment example

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
 import { Link, router } from "expo-router";
 import { View, Text, Pressable } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../state/store";
+import { decrement, increment,incrementByAmount,incrementAsync } from "../../state/counter/counterSlice";
 
 const HomePage = () => {
+  const count = useSelector((state:RootState)=> state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <View>
-      <Text>Home Page</Text>
+      <Text>Count : {count}</Text>
       <Link href="/pkhUsers/1">Go to User 1</Link>
       <Pressable onPress={() => 
         router.push({
@@ -13,6 +19,18 @@ const HomePage = () => {
         })
       }>
         <Text>Go to user 2</Text>
+      </Pressable> 
+      <Pressable onPress={() => dispatch(increment())}>
+        <Text>Click to increase the counter</Text>
+      </Pressable> 
+      <Pressable onPress={() => dispatch(decrement())}>
+        <Text>Click to decrease the counter</Text>
+      </Pressable> 
+      <Pressable onPress={() => dispatch(incrementByAmount(10))}>
+        <Text>Click to increame by 10 the counter</Text>
+      </Pressable> 
+      <Pressable onPress={() => dispatch(incrementAsync(30))}>
+        <Text>Click to increame 30 with delay async</Text>
       </Pressable> 
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,17 @@
 import { Stack } from "expo-router"
+import { Provider } from "react-redux";
+import { store } from "../state/store";
 
 const RootLayout = () => {
-    return <Stack>
-        <Stack.Screen name="(tabs)" options={{
-            headerShown:false
-        }}/>
-    </Stack>
+    return (
+        <Provider store={store}>
+        <Stack>
+            <Stack.Screen name="(tabs)" options={{
+                headerShown:false
+            }}/>
+        </Stack>
+    </Provider>
+    );
 };
 
 export default RootLayout;
