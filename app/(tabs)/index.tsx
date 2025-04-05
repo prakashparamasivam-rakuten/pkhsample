e79bc45d955a932d9225a74eb21dfbe35738c4db
Prakash <iampkh>
2025-04-05 22:39:32 +0900

    adding components structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,10 +3,12 @@ import { View, Text, Pressable } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../state/store";
 import { decrement, increment,incrementByAmount,incrementAsync } from "../../state/counter/counterSlice";
+import MessageItem from "@components/list-item/MessageItem";
 
 const HomePage = () => {
   const count = useSelector((state:RootState)=> state.counter.value);
   const dispatch = useDispatch<AppDispatch>();
+  console.log('hello')
 
   return (
     <View>
@@ -32,6 +34,8 @@ const HomePage = () => {
       <Pressable onPress={() => dispatch(incrementAsync(30))}>
         <Text>Click to increame 30 with delay async</Text>
       </Pressable> 
+      <MessageItem name="Hello" message="this is test" imageUri="https://img.icons8.com/?size=100&id=17950&format=png&color=000000"  />
+      <MessageItem name="HI" message="this is test abc" imageUri="https://img.icons8.com/?size=100&id=17950&format=png&color=000000"  />
     </View>
   );
 };
